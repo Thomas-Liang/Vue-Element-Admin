0add26a2fa521f38d61fbf996861fe12b1bd460e
Thomas Liang <xuanchenyiran@163.com>
2021-12-29 21:24:06 +0800

    feat(refactor):综合作业

--- src/views/team/index.vue
+++ src/views/team/index.vue
@@ -31,95 +31,95 @@
 </template>
 
 <script>
-import { getTeamList } from '@/api/team.js'
-import ListLayout from '@/components/ListLayout/index.vue'
-import AlTable from '@/components/AlTable/index.vue'
-import Conditions from './components/conditions/index.vue'
+import { getTeamList } from "@/api/team.js";
+import ListLayout from "@/components/ListLayout/index.vue";
+import AlTable from "@/components/AlTable/index.vue";
+import Conditions from "./components/conditions/index.vue";
 export default {
   components: {
     ListLayout,
     AlTable,
-    Conditions
+    Conditions,
   },
   data() {
     return {
       formData: {
         column: [
           {
-            prop: 'nick',
-            label: '昵称',
-            sortable: true
+            prop: "nick",
+            label: "昵称",
+            sortable: true,
           },
           {
-            prop: 'phone',
-            label: '手机号',
-            sortable: true
+            prop: "phone",
+            label: "手机号",
+            sortable: true,
           },
           {
-            prop: 'name',
-            label: '真实姓名',
-            sortable: true
+            prop: "name",
+            label: "真实姓名",
+            sortable: true,
           },
           {
-            prop: 'subject',
-            label: '所属学科',
-            sortable: true
+            prop: "subject",
+            label: "所属学科",
+            sortable: true,
           },
           {
-            prop: 'job',
-            label: '职位',
-            sortable: true
+            prop: "job",
+            label: "职位",
+            sortable: true,
           },
           {
-            prop: 'year',
-            label: '工作年限',
-            sortable: true
+            prop: "year",
+            label: "工作年限",
+            sortable: true,
           },
           {
-            prop: 'updateTime',
-            label: '录入时间',
-            sortable: true
+            prop: "updateTime",
+            label: "录入时间",
+            sortable: true,
           },
           {
-            prop: 'actions',
-            label: '操作',
+            prop: "actions",
+            label: "操作",
             render(_, scope) {
               return (
                 <div>
-                  <el-link type='primary'>查看</el-link>
-                  <el-link type='primary'>|</el-link>
-                  <el-link type='primary'>编辑</el-link>
-                  <el-link type='primary'>|</el-link>
-                  <el-link type='primary'>删除</el-link>
+                  <el-link type="primary">查看</el-link>
+                  <el-link type="primary">|</el-link>
+                  <el-link type="primary">编辑</el-link>
+                  <el-link type="primary">|</el-link>
+                  <el-link type="primary">删除</el-link>
                 </div>
-              )
-            }
-          }
+              );
+            },
+          },
         ],
         list: [],
-        temp: []
-      }
-    }
+        temp: [],
+      },
+    };
   },
   created() {
     getTeamList().then((res) => {
-      const { code, data } = res
+      const { code, data } = res;
       if (code === 20000) {
-        const { items } = data
-        this.formData.list = items
-        this.formData.temp = items
+        const { items } = data;
+        this.formData.list = items;
+        this.formData.temp = items;
       }
       // console.log('team', res)
-    })
+    });
   },
   methods: {
     // 获取conditions子组件传来的搜索条件，然后进行列表数据过滤
     searchFilters(payload) {
       // console.log(payload)
       // 1.从payload中获取输入的查询条件
-      const { nick, job, subject, startYear, endYear, phone } = payload
+      const { nick, job, subject, startYear, endYear, phone } = payload;
       // 2.将接口的初始数据赋值给list
-      this.formData.list = this.formData.temp
+      this.formData.list = this.formData.temp;
       if (
         nick === '' &&
         job === '' &&
@@ -141,7 +141,7 @@ export default {
       // console.log(filter)
       // 拿到有值的参数
       const tempFilter = {}
-      Object.keys(filter).forEach(function(key) {
+      Object.keys(filter).forEach(function (key) {
         // console.log(key, filter[key])
         if (
           typeof filter[key] !== undefined &&
@@ -157,13 +157,13 @@ export default {
       // 3.对列表数据进行数据过滤
       const resultArr = this.formData.list.filter((item) => {
         let flag = false
-        Object.keys(tempFilter).forEach(function(key) {
+        Object.keys(tempFilter).forEach(function (key) {
           if (item[key].toString().indexOf(tempFilter[key].toString()) >= 0) {
             flag = true
           } else {
             flag = false
           }
-        })
+        });
         if (flag) {
           return item
         }
